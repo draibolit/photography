ceea6d6202d449be46513f7c3cce5f014d745c2b
tuan <tuannguyenrhm@gmail.com>
2021-02-05 10:25:37 +0900

    Updates

---
+++ precache-manifest.e016f1fddeafd42748050e06707704bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91461ec6ea54fc7139a1f465ba2a955c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c95a2d899d410ef3851",
+    "url": "/static/css/2.beaef9b5.chunk.css"
+  },
+  {
+    "revision": "90bec82a139701e83fcf",
+    "url": "/static/css/main.84d84528.chunk.css"
+  },
+  {
+    "revision": "6c95a2d899d410ef3851",
+    "url": "/static/js/2.935c115c.chunk.js"
+  },
+  {
+    "revision": "a95b6c15d0187e3923b4c682d1d85d6c",
+    "url": "/static/js/2.935c115c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bec82a139701e83fcf",
+    "url": "/static/js/main.80cc254e.chunk.js"
+  },
+  {
+    "revision": "8c608e391879f8e81d09",
+    "url": "/static/js/runtime-main.8ab946aa.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  }
+]);
